2136b09092e88a6ba03898018bd4daf6eea97fea
Anmolbir Mann <amann@ala-lpggp2.wrs.com>
2020-10-23 11:31:45 -0700

    Adding changes for poky x86 qemu

--- hello.c
+++ hello.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
 int main(int argc, char** argv) {
+    char a = 'b';
+    int i = 10;
+    printf("%c\n", a);
+    printf("hey\n");
+    printf("test\n");
     printf("Hello World!\n");
     return 0;
 }
